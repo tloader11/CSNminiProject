dd62ff11e665ea08d688221f81797fa4334bac17
universal <tristan-floyd@hotmail.com>
2017-10-23 13:47:03 +0200

    V1.0 taakverdeling

---
+++ taakverdeling.py
@@ -0,0 +1,7 @@
+sjoerd = "GUI"
+tristan = "serverside + client side + documentatie in python files maken"
+max = "algemene documentatie"
+vincent = "GPIO -> led"
+hidde = "maken presentatie"
+
+#trello -> https://trello.com/b/nzBSnc6p/csn-miniproject

6c78a2aec6c465619d22a915ae977e092f51219a
universal <tristan-floyd@hotmail.com>
2017-10-25 11:28:43 +0200

    Dummy fixed. Now working without errors.

--- php/ClientConnectorDummy.php
+++ php/ClientConnectorDummy.php
@@ -10,21 +10,11 @@ class ClientConnector
         $this->address = "127.0.0.1";
         $this->service_port = 667; //DOOM
 
-        /* Create a TCP/IP socket. */
-        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if ($this->socket === false) {
-            echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-        } else {
-            echo "OK.\n";
-        }
+        echo "OK.\n";
 
         echo "Attempting to connect to '$this->address' on port '$this->service_port'...";
-        $result = socket_connect($this->socket, $this->address, $this->service_port);
-        if ($result === false) {
-            echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($this->socket)) . "\n";
-        } else {
-            echo "OK.\n";
-        }
+        echo "OK.\n";
+
     }
 
     public function TriggerAlarm($type)

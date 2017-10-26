cda3dcc86921abe4105c3861c09013a80d6a1ad9
universal <tristan-floyd@hotmail.com>
2017-10-26 12:03:20 +0200

    ServerSideGPIOimplemented

--- GPIOServerSide.py
+++ GPIOServerSide.py
@@ -30,28 +30,49 @@ lcd_rows    = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 lcd_columns, lcd_rows, lcd_backlight)
 
+triggered = []
 
-def armed():
+def armed(helper):
+    triggered.remove(helper.cID)
+    helper.disarmed_lcd_showed = False
+    if len(triggered) > 0:
+        lcd.clear()
+        s = ""
+        for cID in triggered: s+=str(cID)+" "
+        lcd.message("Clients\nBreached:"+s)
+    else:
+        lcd.clear()
+        lcd.message('All clients\nOK')
+        GPIO.output(ledRood,False)
+        GPIO.output(ledGroen,True)
+
+
+def lcd_text(text):
     lcd.clear()
-    lcd.message('All clients\narmed')
-    GPIO.output(ledRood,False)
-    GPIO.output(ledGroen,True)
+    lcd.message(text)
+
+def disarm(helper):
+    if helper.disarmed_lcd_showed == False:
+        triggered.remove(helper.cID)
+        lcd.message("Client "+helper.cID+"\nDisarmed")
+        helper.disarmed_lcd_showed = True
+        time.sleep(2)
 
 def alarm(helper):
+    triggered.append(helper.cID)
     lcd.clear()
-    lcd.message('Alarm triggerd:\nClient: {}'.format(x))
+    lcd.message('Alarm triggerd:\nClient: {}'.format(helper.cID))
     GPIO.output(ledGroen,False)
-    while True:
-        if helper.armed != True:
-            GPIO.output(ledRood, True)
-            time.sleep(1)
-            GPIO.output(ledRood, False)
-            time.sleep(1)
-        else: break
+    GPIO.output(ledRood, True)
+    time.sleep(1)
+    GPIO.output(ledRood, False)
+    time.sleep(1)
 
+'''
 armed()
 time.sleep(5)
 alarm(3)
 time.sleep(5)
 GPIO.output(ledGroen,False)
 lcd.clear()
+'''
--- csn_server_helper.py
+++ csn_server_helper.py
@@ -2,9 +2,10 @@ import time
 
 import _thread
 
+import GPIOServerSide
 from csn_aes_crypto import csn_aes_crypto
 from packet_processor import *
-#from GPIOServerSide import *
+from GPIOServerSide import *
 
 class ServerHelper:
     c = None
@@ -15,6 +16,7 @@ class ServerHelper:
     aes_encryptor = None
     cID = 0                         #client ID, used for LED color changing
     armed = True
+    disarmed_lcd_showed = False
 
     def __init__(self,c,addr):
         self.c = c
@@ -35,7 +37,22 @@ class ServerHelper:
             self.timer -= 1
         if(self.alarm_triggered == True and self.timer == 0):
             self.PoundAlarm()
+        elif(self.alarm_triggered == False and self.armed == True):
+            lcd_text("All clients\nOK")
+        elif(self.alarm_triggered == False and self.armed == False):
+            lcd_text("Client "+self.cID+"\nDisarmed")
 
     def PoundAlarm(self):
+        alarm(self)
         print("Client",self.cID,"breached! Please investigate!")
         #alarm(self) #needs to be fired in seperate thread, to prevent blocking on main thread of client.
+
+    def ButtonController(self):
+        while True:
+            if self.breached and self.armed:
+                #print("AlarmLoop")
+                GPIOServerSide.alarm(self)
+            elif self.armed:
+                GPIOServerSide.armed(self)
+            elif self.armed == False:
+                GPIOServerSide.disarm(self)

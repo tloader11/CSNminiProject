460db72678e79d71b28230ddc20402e87df558d2
universal <tristan-floyd@hotmail.com>
2017-10-26 20:53:33 +0200

    2e knopje toegevoegd @pin22

--- GPIOClientSide.py
+++ GPIOClientSide.py
@@ -16,6 +16,7 @@ class GPIOClientSide:
     ledGroen = 6
     ledRood = 5
     knopje = 4
+    knopje2 = 22
     breached = False
     armed = True
 
@@ -26,6 +27,7 @@ class GPIOClientSide:
         GPIO.setup(self.ledRood, GPIO.OUT)
         GPIO.setup(self.ledGroen, GPIO.OUT)
         GPIO.setup(self.knopje, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.knopje2, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
     def disarm(self):
         GPIO.output(self.ledRood,False)
@@ -45,7 +47,7 @@ class GPIOClientSide:
 
     def DoButtonCheck(self):
         while True:
-            if GPIO.input(self.knopje) == True:
+            if GPIO.input(self.knopje) == True or GPIO.input(self.knopje2) == True:
                 print(GPIO.input(self.knopje))
                 print("KNOPJE INGEDRUKT -> breached: true")
                 self.breached = True
